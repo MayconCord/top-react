2c7ab17be31230b63551794559e67d4ee3162fd6
MAYCON CORDEIRO CAETANO <20191D12GR0471@LABS.GARANHUNS.IFPE>
2022-08-16 16:20:00 -0300

    commit

---
+++ src/components/Filtro/Filtro.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Filtro() {
+    return(
+        <div className="row">
+            <div className="col-6 col-lg-10">
+                <input inputMode="text"/>
+            </div>
+            <div className="col-3 col-lg-10">
+                <strong>Países: 999</strong>
+            </div>
+            <div className="col-3 col-lg-10">
+                <strong>População: 9.999.999.999</strong>
+            </div>
+        </div>
+    )
+}
